cf9f84489ba822a832c05cb164d4d1a8fe11e68a
Junaid Rasheed <junaidrasheed300@gmail.com>
2021-05-27 20:43:05 +0500

    first commit

---
+++ src/Useformikform.js
@@ -0,0 +1,58 @@
+import { useFormik } from "formik";
+import React from "react";
+
+export const Useformikform = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      age: 0,
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+    validate: (values) => {
+      const error = {};
+      if (!values.name) {
+        error.name = "please enter name";
+      } else if (!values.name.length > 20) {
+        error.name = "Enter name under 20 character";
+      }
+      else if(!values.age){
+          error.age = "Enter age"
+      }
+      return error;
+    },
+  });
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.name}
+          />
+          {formik.errors.name ? (
+            <div style={{ color: "red" }}>{formik.errors.name}</div>
+          ) : null}
+        </div>
+        <div>
+          <label>Age</label>
+          <input
+            id="age"
+            type="number"
+            onChange={formik.handleChange}
+            value={formik.values.age}
+          />
+          {formik.errors.age ? (
+            <div style={{ color: "red" }}>{formik.errors.age}</div>
+          ) : null}
+
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+};
